e17027926358d37578d2e819be23bda5b7ba9ec5
Joshua Gretton <jb.gretton@googlemail.com>
2025-09-18 10:57:46 +0100

    feat(users): updated user model
    Updated models fillables with guardian email and casting with
    last_login_at, role and status

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\Role;
+use App\Enums\Status;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'guardian_email'
     ];
 
     /**
@@ -43,6 +47,9 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'last_login_at' => 'datetime',
+            'role' => Role::class,
+            'status' => Status::class
         ];
     }
 }
